dbd45a76d0b336b51a466f8171f1e75fd33b8862
smatsu <32122648+sara-o-mt2@users.noreply.github.com>
2023-02-18 23:27:59 +0900

    feat: 4381403 code size 1st

--- atcoder_shakyo/abc083_b.py
+++ atcoder_shakyo/abc083_b.py
@@ -1,15 +1,5 @@
 # https://atcoder.jp/contests/abc083/tasks/abc083_b
 
-n, a, b = (int(x) for x in input().split())
-c = 0
-sum = 0
-
-for i in range(1, n + 1):
-    c += 1
-    if a <= c <= b:
-        sum += i
-    while i%10 == 9:
-        c -= 9
-        i //= 10
-
-print(sum)
+n, a, b = map(int,input().split());c=0
+while n:c+=n*(a<=sum(map(int,str(n))))<=b;n-=1
+print(c)
