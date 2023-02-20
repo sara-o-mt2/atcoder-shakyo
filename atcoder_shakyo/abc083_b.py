0fe385df3b1cd28dddffdce451ff8199a5023c67
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2023-02-20 23:39:58 +0900

    fix

--- atcoder_shakyo/abc083_b.py
+++ atcoder_shakyo/abc083_b.py
@@ -1,5 +1,5 @@
 # https://atcoder.jp/contests/abc083/tasks/abc083_b
 
 n, a, b = map(int,input().split());c=0
-while n:c+=n*(a<=sum(map(int,str(n))))<=b;n-=1
+while n:c+=n*(a<=sum(map(int,str(n)))<=b);n-=1
 print(c)
