c2cc61c78ab937bbbb13a2568cd5aa4dc638823d
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2023-02-27 21:29:34 +0900

    feat: 2109022

--- atcoder_shakyo/abc088_b.py
+++ atcoder_shakyo/abc088_b.py
@@ -1,3 +1,27 @@
-N, *A = map(int, open(0).read().split())
-A.sort(reverse=1)
-print(sum(A[::2]) - sum(A[1::2]))
+import sys
+from sys import stdin
+input = stdin.readline
+
+
+def solve(cards):
+    score_a = 0
+    score_b = 0
+    cards.sort(reverse=True)
+    for i, c in enumerate(cards):
+        if i % 2 == 0:
+            score_a += c
+        else:
+            score_b += c
+    return score_a - score_b
+
+
+def main(args):
+    N = int(input())
+    cards = [int(x) for x in input().split()]
+    ans = solve(cards)
+    print(ans)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+    
