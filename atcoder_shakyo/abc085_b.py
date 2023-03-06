64ac495e50e29681d5c090f8f95bd2ba20c3219a
sara-o-mt2 <32122648+sara-o-mt2@users.noreply.github.com>
2023-03-06 23:25:38 +0900

    feat: 1947555

--- atcoder_shakyo/abc085_b.py
+++ atcoder_shakyo/abc085_b.py
@@ -1,1 +1,12 @@
-print(len(set(input() for i in range(int(input())))))
+def main():
+    N = int(input())
+    D = [int(input()) for _ in range(N)]
+ 
+    s = set(D)
+    ans = len(s)
+ 
+    print(ans)
+ 
+ 
+if __name__ == '__main__':
+    main()
